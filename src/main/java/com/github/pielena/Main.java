d396cc7702c2ec184a457cd7f5e138de0222c4df
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2023-05-27 16:24:27 +0300

    implement home task

---
+++ src/main/java/com/github/pielena/Main.java
@@ -0,0 +1,8 @@
+package com.github.pielena;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/github/pielena/container/Box.java
@@ -0,0 +1,49 @@
+package com.github.pielena.container;
+
+import com.github.pielena.entity.Fruit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T extends Fruit> {
+
+    private List<T> fruits = new ArrayList<>();
+
+    public Box() {
+    }
+
+    public Box(List<T> fruits) {
+        this.fruits = fruits;
+    }
+
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public double weight() {
+        return fruits.stream()
+                .mapToDouble(Fruit::getWeight)
+                .sum();
+    }
+
+    public boolean compare(Box<?> another) {
+        if (another == null) {
+            return false;
+        }
+        return Math.abs(this.weight() - another.weight()) < 0.001f;
+    }
+
+    public void transferFruits(Box<? super T> destBox) {
+        if (destBox == null) {
+            throw new NullPointerException("DestBox can't be null");
+        }
+        if (destBox != this) {
+            destBox.getFruits().addAll(fruits);
+            fruits.clear();
+        }
+    }
+
+    public List<T> getFruits() {
+        return fruits;
+    }
+}
